9646dbb1f06bfaa0fcafdff8529193eb58c838cf
Vadim Korotov <mpcomp12@gmail.com>
2020-08-05 23:48:45 +0300

    Hook script refactoring

--- utils/addTrelloHook.js
+++ utils/addTrelloHook.js
@@ -4,20 +4,20 @@ const {
   TRELLO_LIST_ID,
   TRELLO_TOKEN,
   TRELLO_KEY,
-  npm_config_url } = process.env;
+  NETLIFY_BUILD_URL } = process.env;
 
 const fetch = require('node-fetch');
 
 const body = {
   description: "Netlify build hook",
-  callbackURL: npm_config_url,
+  callbackURL: NETLIFY_BUILD_URL,
   idModel: TRELLO_LIST_ID
 }
 
 fetch(`https://api.trello.com/1/tokens/${TRELLO_TOKEN}/webhooks/?key=${TRELLO_KEY}`, {
-    method: 'post',
-    body: JSON.stringify(body),
-    headers: { 'Content-Type': 'application/json' },
-  })
+  method: 'post',
+  body: JSON.stringify(body),
+  headers: { 'Content-Type': 'application/json' },
+})
   .then(res => res.json())
   .then(json => console.log(json));
